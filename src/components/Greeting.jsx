54ca8b7bc84755ac92f244f02afeb215a00f5116
shahan <shahan@gmail.com>
2023-03-17 03:31:33 +0500

    fix difference in greeting value between server and client renders

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Greeting({messages}) {
   const randomMessage= () => messages[(Math.floor(Math.random() * messages.length))]
-  const [greeting, setGreeting] = useState(randomMessage())
+  const [greeting, setGreeting] = useState()
+  useEffect(() => {
+    setGreeting(randomMessage())
+  },[])
   function handleGreeting() {
     setGreeting(randomMessage())
   }
